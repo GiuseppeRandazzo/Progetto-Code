1e4f9c6ae361829f7e30812f913ef64fcdffe75b
Giuseppe Randazzo <giupeppolo21@gmail.com>
2024-06-27 16:27:21 +0200

    app marketplace development

--- marketplace.js
+++ marketplace.js
@@ -1,10 +1,3 @@
-class Marketplace {
-  users = [];
-  ads = [];
-  reviews = [];
-  auth = [];
-}
-
 class ModelUser {
   constructor(username, email, password) {
     this.id = Math.random();
@@ -16,7 +9,7 @@ class ModelUser {
 
 const user = new ModelUser("nome", "email", "password");
 
-class ModelUsers {
+class ModelUser {
   constructor(username, email, password) {
     this.primarykey = Math.random();
     this.username = username;
@@ -25,13 +18,13 @@ class ModelUsers {
   }
 }
 
-const utente = new ModelUtente(
-  "Giuseppe",
-  "giusepperandazzo@gmail.com",
+const User = new ModelUser(
+  "GiuseppeRandazzo",
+  "GiuseppeRandazzo@gmail.com",
   "1234"
 );
 
-class ModelAds {
+class ModelAd {
   constructor(referencekeyUser, title, description, price, category, urlphoto) {
     this.primarykey = Math.random();
     this.referencekeyUser = referencekeyUser;
@@ -43,7 +36,7 @@ class ModelAds {
   }
 }
 
-const ads = new ModelAds(
+const Ad = new ModelAd(
   "utente",
   "title",
   "description",
@@ -52,7 +45,7 @@ const ads = new ModelAds(
   "urlphoto"
 );
 
-class ModelReviews {
+class ModelReview {
   constructor(referencekeyUser, referencekeyAd, title, description, rating) {
     this.primarykey = Math.random();
     this.referencekeyUser = referencekeyUser;
@@ -64,7 +57,7 @@ class ModelReviews {
   }
 }
 
-const reviews = new ModelReviews(
+const Review = new ModelReview(
   "utente",
   "ads",
   "title",
@@ -80,9 +73,7 @@ class ModelAuth {
   }
 }
 
-const auth = new ModelAuth("utente");
-
-class ModelReports {
+class ModelReport {
   constructor(idUser, idAd, description, status) {
     this.primarykey = Math.random();
     this.referencekeyUser = idUser;
@@ -92,12 +83,115 @@ class ModelReports {
   }
 }
 
-const reports = new ModelReports("utente", "ads", "description", "status");
-
-class modelFavorites {
+class modelFavorite {
   constructor(idUser, idAd) {
     this.primarykey = Math.random();
     this.referencekeyUser = idUser;
     this.referencekeyAd = idAd;
   }
 }
+
+class ModelDevice {
+  constructor(idUser, idAd) {
+    this.primarykey = Math.random();
+    this.referencekeyUser = idUser;
+    this.referencekeyAd = idAd;
+  }
+}
+
+class Marketplace {
+  users = [];
+  ad = [];
+  review = [];
+  auth = [];
+  report = [];
+  favorite = [];
+  device = [];
+
+  login(email, password) {
+    const userfound = this.users.find(function (user) {
+      if (user.email === email && user.password === password) return true;
+      else return false;
+    });
+    if (!!userfound) {
+      const newAuth = new ModelAuth(userfound.primaryKeyUser);
+      this.auth = [...this.auth, newAuth];
+      return newAuth.token;
+    } else console.log("Ma chi minchia sei?");
+  }
+
+  logout(token) {
+    const authFound = this.auth.find(function (auth) {
+      if (auth.token === token) return true;
+      else return false;
+    });
+    if (!!authFound) {
+      this.auth = this.auth.find(function (auth) {
+        if (auth.token === token) return false;
+        else return true;
+      });
+      console.log("logout effettuato con successo");
+    } else console.log("token non valido");
+  }
+
+  register(email, password) {
+    const userfound = this.users.find(function (user) {
+      if (user.email === email) return true;
+      else return false;
+    });
+
+    if (!!userfound) console.log("utente gia registrato");
+    else {
+      const newUser = new ModelUser(email, email, password);
+      this.users = [...this.users, newUser];
+      console.log("Registrazione effettuata con successo");
+    }
+  }
+
+  isTokenValid(token) {
+    const authFound = this.auth.find(function (auth) {
+      if (auth.token === token) return true;
+      else return false;
+    });
+    if (!!authFound) return true;
+    else return false;
+  }
+
+  getAuthUserbytoken(token) {
+    const authFound = this.auth.find(function (auth) {
+      if (auth.token === token) return true;
+      else return false;
+    });
+    if (!!authFound) return authFound;
+    else return null;
+  }
+
+  addFavorite(token, referencekeyUser, referencekeyAd) {
+    const userAuth = this.getAuthUserbytoken(token);
+    if (!userAuth) console.log("Token non valido");
+    else {
+      const newFavorite = new modelFavorite(
+        uaserAuth,
+        referencekeyUser,
+        referencekeyAd
+      );
+      this.favorite = [...this.favorite, newFavorite];
+      console.log("aggiunto ai preferiti");
+    }
+  }
+}
+createad(referencekeyUser, title, description, price, category, urlphoto);
+
+modifiedad(referencekeyUser, title, description, price, category, urlphoto);
+
+deletead(referencekeyUser, title, description, price, category, urlphoto);
+
+createreview(referencekeyUser, referencekeyAd, title, description, rating);
+
+modifiedreview(referencekeyUser, referencekeyAd, title, description, rating);
+
+deletereview(referencekeyUser, referencekeyAd, title, description, rating);
+
+deleteaccount(referencekeyUser);
+
+modfiedusername(referencekeyUser, username);
